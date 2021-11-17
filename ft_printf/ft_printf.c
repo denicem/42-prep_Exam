fc3e4b5c48b6e7335a48f93e4f711a222bbb7daf
Denice Montemayor <dmontema@3-F-3.42heilbronn.de>
2021-11-17 22:25:03 +0100

    add ft_printf.c.

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/17 20:52:03 by dmontema          #+#    #+#             */
+/*   Updated: 2021/11/17 22:22:45 by dmontema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <stdio.h>
+
+////////////////////////////////////////
+////////////** PRINTERS **/////////////
+
+int ft_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int ft_putstr(char *s)
+{
+	int res;
+
+	if (s == NULL)
+		return (write(1, "(null)", 6));
+	res = 0;
+	while (*s)
+	{
+		res += ft_putchar(*s);
+		s++;
+	}
+	return (res);
+}
+
+int ft_putnbr(int n)
+{
+	int res;
+
+	res = 0;
+	if (n == INT_MIN)
+	{
+		res += write(1, "-2", 2);
+		n *= -1;
+		n /= 10;
+	}
+	if (n < 0)
+	{
+		res += write (1, "-", 1);
+		n *= -1;
+	}
+	if (n > 9)
+		res += ft_putnbr(n / 10);
+	res += ft_putchar((n % 10) + 48);
+	return (res);
+}
+
+int ft_putnbr_base(unsigned long n, int base, char *set)
+{
+	int res;
+
+	res = 0;
+	if (n > (unsigned long) base)
+		res += ft_putnbr_base(n / base, base, set);
+	res += write(1, &set[n%base], 1);
+	return (res);
+}
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+int select_conv_spec(char format, va_list args)
+{
+	if (format == 0)
+		return (0);
+	if (format == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	else if (format == 'd' || format == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	else if (format == 'x')
+		return (ft_putnbr_base(va_arg(args, unsigned long), 16, "0123456789abcdef"));
+	return (ft_putchar(format));
+}
+
+int	ft_printf(char *format, ...)
+{
+	int res;
+	va_list args;
+
+	res = 0;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 0)
+				break ;
+			res += select_conv_spec(*format, args);
+		}
+		else 
+			res += ft_putchar(*format);
+		format++;
+	}
+	va_end(args);
+	return (res);
+}
+
+int main()
+{
+	int out = printf("%s %d %x%%%\n%", "Hello", 42, 15343);
+	int out1 = ft_printf("%s %d %x%%%\n%", "Hello", 42, 15343);
+
+	printf("\n%d | %d\n", out, out1);
+
+	return (0);
+}
