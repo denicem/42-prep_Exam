7cb47b3d229aeb1b0f736f5438f26f3eb79f7c92
Denice Montemayor <dmontema@1-D-15.42heilbronn.de>
2021-12-16 17:01:55 +0100

    recoded last attempt for ft_printf.

--- ER02/ft_printf.c
+++ ER02/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 20:52:03 by dmontema          #+#    #+#             */
-/*   Updated: 2021/11/17 23:01:36 by dmontema         ###   ########.fr       */
+/*   Updated: 2021/12/16 16:19:16 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,73 +19,62 @@
 ////////////////////////////////////////
 ////////////** PRINTERS **/////////////
 
-int ft_putchar(char c)
+int ft_putchar(unsigned char c)
 {
 	return (write(1, &c, 1));
 }
 
-int ft_putstr(char *s)
+int ft_putstr(char *str)
 {
-	int res;
+	int res = 0;
 
-	if (s == NULL)
-		return (write(1, "(null)", 6));
-	res = 0;
-	while (*s)
+	if (str == 0)
+		res += write(1, "(null)", 6);
+	else
 	{
-		res += ft_putchar(*s);
-		s++;
+		while (*str)
+		{
+			res += ft_putchar(*str);
+			str++;
+		}
+
 	}
 	return (res);
 }
 
-int ft_putnbr(int n)
+int ft_putnbr(int nbr)
 {
-	int res;
+	int res = 0;
 
-	res = 0;
-	if (n == INT_MIN)
+	if (nbr == INT_MIN)
 	{
 		res += write(1, "-2", 2);
-		n = 147483648;
+		nbr = 147483648;
 	}
-	if (n < 0)
+	if (nbr < 0)
 	{
 		res += write (1, "-", 1);
-		n *= -1;
+		nbr *= -1;
 	}
-	if (n > 9)
-		res += ft_putnbr(n / 10);
-	res += ft_putchar((n % 10) + 48);
+	if (nbr > 9)
+		res += ft_putnbr(nbr / 10);
+	res += ft_putchar(48 + (nbr % 10));
 	return (res);
 }
 
-int ft_putnbr_base(unsigned long n, int base, char *set)
+int ft_putnbr_base(unsigned long long nbr, int base, char *set)
 {
 	int res;
 
 	res = 0;
-	if (n > (unsigned long) base)
-		res += ft_putnbr_base(n / base, base, set);
-	res += write(1, &set[n%base], 1);
+	if (nbr >= (unsigned long long) base)
+		res += ft_putnbr_base(nbr / base, base, set);
+	res += ft_putchar(set[nbr % base]);
 	return (res);
 }
 
 ////////////////////////////////////////
 ////////////////////////////////////////
-
-int select_conv_spec(char format, va_list args)
-{
-	if (format == 0)
-		return (0);
-	if (format == 's')
-		return (ft_putstr(va_arg(args, char *)));
-	else if (format == 'd' || format == 'i')
-		return (ft_putnbr(va_arg(args, int)));
-	else if (format == 'x')
-		return (ft_putnbr_base(va_arg(args, unsigned long), 16, "0123456789abcdef"));
-	return (ft_putchar(format));
-}
 
 int	ft_printf(char *format, ...)
 {
@@ -101,7 +90,14 @@ int	ft_printf(char *format, ...)
 			format++;
 			if (*format == 0)
 				break ;
-			res += select_conv_spec(*format, args);
+			if (*format == 's')
+				res += ft_putstr(va_arg(args, char *));
+			else if (*format == 'd')
+				res += ft_putnbr(va_arg(args, int));
+			else if (*format == 'x')
+				res += ft_putnbr_base(va_arg(args, unsigned long long), 16, "0123456789abcdef");
+			else if (*format == '%')
+				res += ft_putchar(*format);
 		}
 		else 
 			res += ft_putchar(*format);
@@ -113,10 +109,16 @@ int	ft_printf(char *format, ...)
 
 int main()
 {
-	int out = printf("%d\n", INT_MIN);
-	int out1 = ft_printf("%d\n", INT_MIN);
-
-	printf("\n%d | %d\n", out, out1);
+	ft_printf("Hello, %s!\n", "42Heilbronn");
+	ft_printf("nbr: %d, neg nbr: %d, hex: %x\n", 42, -42, 42);
+	ft_printf("%s\n", NULL);
+	ft_printf("---------------\n");
+	/* <<<<<<< INT_MIN && INT_MAX >>>>>>> */
+	// ft_printf("MY:\tINT_MIN: %d | INT_MAX: %d\n", INT_MIN, INT_MAX);
+	// printf("OG:\tINT_MIN: %d | INT_MAX: %d\n", INT_MIN, INT_MAX);
+	// ft_printf("\n");
+	// ft_printf("MY:\tINT_MIN-1: %d | INT_MAX+1: %d\n", INT_MIN - 1, INT_MAX) + 1;
+	// printf("OG:\tINT_MIN-1: %d | INT_MAX+1: %d\n", INT_MIN - 1, INT_MAX) + 1;
 
 	return (0);
 }
