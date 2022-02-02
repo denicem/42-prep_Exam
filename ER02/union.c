be27be60aa87f3d028d450186d2a9abf2f053146
Denice M <denicem@hotmail.de>
2022-02-03 00:20:36 +0100

    implement own ver of union.c.

--- ER02/union.c
+++ ER02/union.c
@@ -12,40 +12,41 @@
 
 #include <unistd.h>
 
-// TODO:	implement own ver
-// Olawale's ver 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	unsigned char x[256];
-	int i = 0;
 	if (argc == 3)
 	{
-		while (i++ < 256)
-			x[i-1] = 0;
+		int chars[256];
+		int i = 0;
+
+		while (i < 256)
+			chars[i++] = 0;
 		i = 0;
-		while (argv[1][i++])
+		while (argv[1][i])
 		{
-			if (x[(unsigned char)(argv[1][i - 1])] == 0)
+			if (chars[(unsigned char)argv[1][i]] == 0)
 			{
-				x[(unsigned char)(argv[1][i-1])] = 1;
-				write(1, &(argv[1][i-1]), 1);
+				chars[(unsigned char)argv[1][i]] = 1;
+				write(1, &argv[1][i], 1);
+				i++;
 			}
+			else
+				i++;
 		}
 		i = 0;
-		while (argv[2][i++])
+		while (argv[2][i])
 		{
-			if (x[(unsigned char)(argv[2][i - 1])] == 0)
+			if (chars[(unsigned char)argv[2][i]] == 0)
 			{
-				x[(unsigned char)(argv[2][i-1])] = 1;
-				write(1, &(argv[2][i-1]), 1);
+				chars[(unsigned char)argv[2][i]] = 1;
+				write(1, &argv[2][i], 1);
+				i++;
 			}
+			else
+				i++;
 		}
 
 	}
-	write (1, "\n", 1);
-	return 0;
+	write(1, "\n", 1);
+	return (0);
 }
-
-// TODO:	also implement String intersection.
-// TODO:	divide into different sections and implement a selection mode to select one of the funcs (with the help of argv)
-// TODO:	rename this .c-file
